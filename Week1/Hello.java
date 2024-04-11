d51031cfb7042e64f961860b875471bc90cc9e27
das-bhogendra <161198387+das-bhogendra@users.noreply.github.com>
2024-04-11 19:04:25 +0545

    this is 2nd question ans

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -4,5 +4,8 @@ public class Hello{
     public static void main(String[] args) {
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.print("Hello World\nThis is my first program\nI am on module ST4003CEM");
+        System.out.println("A\"quoted\"string is");
+        System.out.println("\'much better\'if you learn\n\'rule of escape sequence'");
+        System.out.println("instead of \" !\n \"is not the same as\"");
     }
 }
---
+++ Week1/Task1.java
@@ -0,0 +1,8 @@
+package Week1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("A\"quoted\"string is");
+    }
+    
+}
