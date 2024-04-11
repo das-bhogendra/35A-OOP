bc8ddd3e278d389d0e9c1e3a659e0d46f6f295cf
das-bhogendra <161198387+das-bhogendra@users.noreply.github.com>
2024-04-11 19:11:03 +0545

    ans of 3rd question

--- Week1/Task1.java
+++ Week1/Task1.java
@@ -3,6 +3,8 @@ package Week1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("A\"quoted\"string is");
+        System.out.println("\'much better\'if you learn\n\'rule of escape sequence'");
+        System.out.println("instead of \" !\n \"is not the same as\"");
     }
     
 }
